4ef3e63641215a45592838c28dd75497f83e7d72
Jake Mays <jake.m.mays@gmail.com>
2018-01-02 23:18:45 -0700

    --amend

--- apps/demarest_mays_web/config/prod.exs
+++ apps/demarest_mays_web/config/prod.exs
@@ -19,13 +19,6 @@ config :demarest_mays_web, DemarestMaysWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
-config :demarest_mays_web, DemarestMaysWeb.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL"),
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-  ssl: true
-
-
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
